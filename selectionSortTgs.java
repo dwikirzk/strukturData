013f25f326c55a2b0c3d5363523d404193f3f0dd
dwikirzk <kidwi142536@gmail.com>
2026-03-08 10:30:28 +0700

    Upload pertama tugas struktur data

---
+++ selectionSortTgs.java
@@ -0,0 +1,33 @@
+package strukturData;
+
+public class selectionSortTgs {
+  public static void main(String[] args) {
+    int[] x = { 20, 15, 90, 13, 26 };
+
+    for (int i = 0; i < x.length - 1; i++) { // Loop untuk melakukan 4 kali iterasi
+      int min = 999999;
+      int index = i;
+
+      for (int j = i; j < x.length; j++) { // Loop untuk mencari nilai terkecil
+        if (x[j] < min) {
+          min = x[j];
+          index = j;
+        }
+      }
+
+      // Proses Tukar (Swap)
+      if (x[i] > x[index]) {
+        int temp = 0;
+        temp = x[i];
+        x[i] = x[index];
+        x[index] = temp;
+      }
+
+      // Cetak Hasil Array setelah ditukar
+      for (int b = 0; b < x.length; b++) {
+        System.out.print(x[b] + " ");
+      }
+      System.out.println();
+    }
+  }
+}
